d8a1c727ba4bf0ad6e3c423280caf32ba4b1ac2f
Claudivan <ivamsferreira@gmail.com>
2023-02-04 15:55:24 -0300

    Alterando os dados

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -1,50 +1,69 @@
-const CarroService = require('../services/CarroService');
+const CarroService = require("../services/CarroService");
 
 module.exports = {
-    buscarTodos: async (req, res)=> {
-        let json = {error:'', result:[]};
+  buscarTodos: async (req, res) => {
+    let json = { error: "", result: [] };
 
-        let carros =  await CarroService.buscarTodos();
+    let carros = await CarroService.buscarTodos();
 
-        for(let i in carros){
-            json.result.push({
-                codigo: carros[i].codigo,
-                descricao: carros[i].modelo
-            });
-        }
-        res.json(json);
-    },
+    for (let i in carros) {
+      json.result.push({
+        codigo: carros[i].codigo,
+        descricao: carros[i].modelo,
+      });
+    }
+    res.json(json);
+  },
 
-    buscarUm: async(req, res) => {
-        let json = {error:'', result:{}};
+  buscarUm: async (req, res) => {
+    let json = { error: "", result: {} };
 
-        let codigo = req.params.codigo;
-        let carro = await CarroService.buscarUm(codigo);
+    let codigo = req.params.codigo;
+    let carro = await CarroService.buscarUm(codigo);
 
-        if(carro){
-            json.result = carro;
-        }
-        res.json(json);
-    
-},
+    if (carro) {
+      json.result = carro;
+    }
+    res.json(json);
+  },
 
-     inserir: async(req, res) => {
-         let json = {error:'', result:{}};
+  inserir: async (req, res) => {
+    let json = { error: "", result: {} };
 
-         let modelo = req.body.modelo;
-         let placa = req.body.placa;
-        
+    let modelo = req.body.modelo;
+    let placa = req.body.placa;
 
-         if(modelo && placa){
-            let CarroCodigo = await CarroService.inserir(modelo, placa);
-            json.result = {
-                codigo: CarroCodigo,
-                modelo,
-                placa
-            };
-         }else{
-            json.error = 'Campos não enviados';
-         }
-         res.json(json);
+    if (modelo && placa) {
+      let CarroCodigo = await CarroService.inserir(modelo, placa);
+      json.result = {
+        codigo: CarroCodigo,
+        modelo,
+        placa,
+      };
+    } else {
+      json.error = "Campos não enviados";
+    }
+    res.json(json);
+  },
+
+
+    alterar: async (req, res) => {
+      let json = { error:"", result:{}};
+
+      let codigo = req.params.codigo;
+      let modelo = req.body.modelo;
+      let placa = req.body.placa;
+
+      if (codigo && modelo && placa) {
+         await CarroService.alterar(codigo, modelo,placa);
+        json.result = {
+        codigo,
+        modelo,
+        placa,
+        };
+    }else{
+        json.error = "Campos não enviados";
+  }
+      res.json(json);
     }
 }
